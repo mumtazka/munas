41cdab4f9c40e3f3ecb779a4e763199615ce43f5
Mumtaz Kholafiyan Alfan <mumtazalfan1307@gmail.com>
2026-02-06 18:02:38 +0700

    Refactor: Simplify structure, remove backend, move frontend to root

---
+++ craco.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  eslint: {
+    configure: {
+      extends: ["plugin:react-hooks/recommended"],
+      rules: {
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+      },
+    },
+  },
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    configure: (webpackConfig) => {
+      // Add ignored patterns to reduce watched directories
+      webpackConfig.watchOptions = {
+        ...webpackConfig.watchOptions,
+        ignored: [
+          '**/node_modules/**',
+          '**/.git/**',
+          '**/build/**',
+          '**/dist/**',
+          '**/coverage/**',
+          '**/public/**',
+        ],
+      };
+      return webpackConfig;
+    },
+  },
+};
